f3f88590552bf47f898881b34b7156208e4e9a86
Binod Rasaili <binod.toyam@gmail.com>
2024-08-05 20:51:00 -0500

    fixed flightAdd

--- src/pages/admin/flight/FlightAdd.js
+++ src/pages/admin/flight/FlightAdd.js
@@ -1,5 +1,5 @@
 import { useFormik } from 'formik';
-import { Input, Button, Form, Dropdown } from 'rsuite';
+import { AutoComplete, Button, Form } from 'rsuite';
 import InputField from "../../../components/InputField";
 import axios from "../../../configs/axios";
 import { toast } from "react-toastify";
@@ -7,11 +7,14 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { setQuery } from "../../../features/flightQuerySlice";
+import { useDispatch } from "react-redux";
 
 const FlightAdd = () => {
     const [airports, setAirports] = useState([]);
     const [selectedDepartureAirportId, setSelectedDepartureAirportId] = useState('');
     const [selectedArrivalAirportId, setSelectedArrivalAirportId] = useState('');
+    const dispatch = useDispatch();
 
     useEffect(() => {
         getAirportData();
@@ -19,10 +22,33 @@ const FlightAdd = () => {
 
     const getAirportData = () => {
         axios.get(`/airports`).then(res => {
-            setAirports(res.data);
+            const airportData = res.data.map(row => ({
+                id: row.id,
+                code: row.code,
+                name: `${row.code} - ${row.name}, ${row.city}, ${row.country}`
+            }));
+            setAirports(airportData);
         }).catch(err => {
             console.log(err);
         });
+    };
+
+    const searchFrom = (value) => {
+        const airportCode = value.split("-")[0].trim();
+        const selectedAirport = airports.find(data => data.code === airportCode);
+        if (selectedAirport) {
+            setSelectedDepartureAirportId(selectedAirport.id);
+            dispatch(setQuery({ departureAirportId: selectedAirport.id }));
+        }
+    };
+
+    const searchTo = (value) => {
+        const airportCode = value.split("-")[0].trim();
+        const selectedAirport = airports.find(data => data.code === airportCode);
+        if (selectedAirport) {
+            setSelectedArrivalAirportId(selectedAirport.id);
+            dispatch(setQuery({ arrivalAirportId: selectedAirport.id }));
+        }
     };
 
     const navigate = useNavigate();
@@ -77,7 +103,6 @@ const FlightAdd = () => {
             return errors;
         },
         onSubmit: data => {
-            // Convert availableSeats to integer and ticketPrice to float
             const payload = {
                 flightNumber: data.flightNumber,
                 departureAirport: { id: selectedDepartureAirportId },
@@ -88,135 +113,142 @@ const FlightAdd = () => {
                     departureTime: data.flightSchedule.departureTime.toTimeString().split(' ')[0],
                     arrivalTime: data.flightSchedule.arrivalTime.toTimeString().split(' ')[0]
                 },
-                availableSeats: parseInt(data.availableSeats, 10), // Convert to integer
+                availableSeats: parseInt(data.availableSeats, 10),
                 flightLogo: data.flightLogo,
-                ticketPrice: parseInt(data.ticketPrice) // Convert to float
+                ticketPrice: parseFloat(data.ticketPrice)
             };
 
             axios.post("/flights/admin", payload)
-                .then(res => {
+                .then(() => {
                     navigate("/admin/flight/list");
                     toast.success("Successfully saved!");
                 });
         }
     });
 
-    const handleDropdownSelect = (value, type) => {
-        if (type === 'departure') {
-            setSelectedDepartureAirportId(value);
-            formik.setFieldValue('departureAirport', { id: value });
-        } else if (type === 'arrival') {
-            setSelectedArrivalAirportId(value);
-            formik.setFieldValue('arrivalAirport', { id: value });
-        }
-    };
-
     return (
         <div className={`box`}>
             <Form className={`w-100`} onSubmit={formik.handleSubmit}>
                 <InputField
                     name="flightNumber"
-                    placeholder="Flight Number"
+                    placeholder="Enter Flight Number"
                     value={formik.values.flightNumber}
                     error={formik.errors.flightNumber}
                     onChange={value => formik.setFieldValue('flightNumber', value)}
                 />
+                <div className={`row`}>
+                    <div className={`col-4`}>
+                        <AutoComplete
+                            placeholder="Departure Airport"
+                            data={airports.map(item => item.name)}
+                            onSelect={searchFrom}
+                        />
+                    </div>
+                    <div className={`col-4`}>
+                        <AutoComplete
+                            placeholder="Arrival Airport"
+                            data={airports.map(item => item.name)}
+                            onSelect={searchTo}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <br/>
+                </div>
+                <div className={`row`}>
+                    <div className={`col-4`}>
+                        <InputField
+                            name="availableSeats"
+                            type="number"
+                            placeholder="Available Seats"
+                            value={formik.values.availableSeats}
+                            error={formik.errors.availableSeats}
+                            onChange={value => formik.setFieldValue('availableSeats', value)}
+                        />
+                    </div>
+                    <div className={`col-4`}>
 
-                <Dropdown
-                    title={airports.find(airport => airport.id === selectedDepartureAirportId)?.name || "Select Departure Airport"}
-                    onSelect={(value) => handleDropdownSelect(value, 'departure')}
-                >
-                    {
-                        airports.map(item => (
-                            <Dropdown.Item key={item.id} eventKey={item.id}>
-                                {item.name}
-                            </Dropdown.Item>
-                        ))
-                    }
-                </Dropdown>
+                        <InputField
+                            name="flightLogo"
+                            placeholder="Flight Logo URL"
+                            value={formik.values.flightLogo}
+                            error={formik.errors.flightLogo}
+                            onChange={value => formik.setFieldValue('flightLogo', value)}
+                        />
+                    </div>
+                    <div className={`col-4`}>
+                        <InputField
+                            name="ticketPrice"
+                            type="number"
+                            placeholder="Ticket Price"
+                            value={formik.values.ticketPrice}
+                            error={formik.errors.ticketPrice}
+                            onChange={value => formik.setFieldValue('ticketPrice', value)}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <br/>
+                </div>
 
-                <Dropdown
-                    title={airports.find(airport => airport.id === selectedArrivalAirportId)?.name || "Select Arrival Airport"}
-                    onSelect={(value) => handleDropdownSelect(value, 'arrival')}
-                >
-                    {
-                        airports.map(item => (
-                            <Dropdown.Item key={item.id} eventKey={item.id}>
-                                {item.name}
-                            </Dropdown.Item>
-                        ))
-                    }
-                </Dropdown>
+                <div className={`row`}>
+                    <span>Select Departure Date and time</span>
+                    <div className={`col-4`}>
+                        <DatePicker
+                            selected={formik.values.flightSchedule.departureDate}
+                            onChange={(date) => formik.setFieldValue('flightSchedule.departureDate', date)}
+                            placeholderText="Departure Date"
+                            dateFormat="yyyy-MM-dd"
 
-                <InputField
-                    name="availableSeats"
-                    type="number"
-                    placeholder="Available Seats"
-                    value={formik.values.availableSeats}
-                    error={formik.errors.availableSeats}
-                    onChange={value => formik.setFieldValue('availableSeats', value)}
-                />
+                        />
+                    </div>
+                    <div className={`col-4`}>
+                        <DatePicker
+                            selected={formik.values.flightSchedule.departureTime}
+                            onChange={(date) => formik.setFieldValue('flightSchedule.departureTime', date)}
+                            placeholderText="Departure Time"
+                            showTimeSelect
+                            showTimeSelectOnly
+                            timeFormat="HH:mm"
+                            timeIntervals={15}
+                            dateFormat="HH:mm"
+                        />
+                    </div>
+                </div>
+                <div className={`row`}>
+                    <span>Select Arrival Date and time</span>
+                    <div className={`col-4`}>
+                        <DatePicker
+                            selected={formik.values.flightSchedule.arrivalDate}
+                            onChange={(date) => formik.setFieldValue('flightSchedule.arrivalDate', date)}
+                            placeholderText="Arrival Date"
+                            dateFormat="yyyy-MM-dd"
+                        />
+                    </div>
+                    <div className={`col-4`}>
+                        <DatePicker
+                            selected={formik.values.flightSchedule.arrivalTime}
+                            onChange={(date) => formik.setFieldValue('flightSchedule.arrivalTime', date)}
+                            placeholderText="Arrival Time"
+                            showTimeSelect
+                            showTimeSelectOnly
+                            timeFormat="HH:mm"
+                            timeIntervals={15}
+                            dateFormat="HH:mm"
+                        />
+                    </div>
+                </div>
 
-                <InputField
-                    name="flightLogo"
-                    placeholder="Flight Logo URL"
-                    value={formik.values.flightLogo}
-                    error={formik.errors.flightLogo}
-                    onChange={value => formik.setFieldValue('flightLogo', value)}
-                />
-
-                <InputField
-                    name="ticketPrice"
-                    type="number"
-                    placeholder="Ticket Price"
-                    value={formik.values.ticketPrice}
-                    error={formik.errors.ticketPrice}
-                    onChange={value => formik.setFieldValue('ticketPrice', value)}
-                />
-
-                <DatePicker
-                    selected={formik.values.flightSchedule.departureDate}
-                    onChange={(date) => formik.setFieldValue('flightSchedule.departureDate', date)}
-                    placeholderText="Departure Date"
-                    dateFormat="yyyy-MM-dd"
-                />
-                <DatePicker
-                    selected={formik.values.flightSchedule.arrivalDate}
-                    onChange={(date) => formik.setFieldValue('flightSchedule.arrivalDate', date)}
-                    placeholderText="Arrival Date"
-                    dateFormat="yyyy-MM-dd"
-                />
-                <DatePicker
-                    selected={formik.values.flightSchedule.departureTime}
-                    onChange={(date) => formik.setFieldValue('flightSchedule.departureTime', date)}
-                    placeholderText="Departure Time"
-                    showTimeSelect
-                    showTimeSelectOnly
-                    timeFormat="HH:mm"
-                    timeIntervals={15}
-                    dateFormat="HH:mm"
-                />
-                <DatePicker
-                    selected={formik.values.flightSchedule.arrivalTime}
-                    onChange={(date) => formik.setFieldValue('flightSchedule.arrivalTime', date)}
-                    placeholderText="Arrival Time"
-                    showTimeSelect
-                    showTimeSelectOnly
-                    timeFormat="HH:mm"
-                    timeIntervals={15}
-                    dateFormat="HH:mm"
-                />
-
-                <Button
-                    className={`float-end`}
-                    appearance="default"
-                    type="submit"
-                >
-                    Submit
-                </Button>
+                        <Button
+                            className={`float-end`}
+                            appearance="default"
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
             </Form>
         </div>
-    );
+);
 };
 
 export default FlightAdd;
