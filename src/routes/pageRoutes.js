364b2a53f6c5c06f5160548198b268afc6230a61
Abdoon Nur <abir023048@gmail.com>
2024-08-15 09:15:54 -0500

    css updated

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -4,16 +4,21 @@ import Register from "../pages/Register";
 import LandingPage from "../pages/LandingPage";
 import FlightBooking from "../pages/FlightBooking";
 import MyBookings from "../pages/MyBookings";
+import Dashboard from "../pages/admin/Dashboard";
+import PrivateRoute from "./privateRoute";
 
 
 const PageRoutes = () => {
 
     return <Routes>
-        <Route path={"/"} element={<LandingPage />}/>
+        <Route path={"/"} element={<LandingPage/>}/>
         <Route path={"/login"} element={<Login/>}/>
         <Route path={"/register"} element={<Register/>}/>
         <Route path={"/flight-booking"} element={<FlightBooking/>}/>
-        <Route path={"/my-bookings"} element={<MyBookings/>}/>
+        <Route path={"/my-bookings"} element={<PrivateRoute roles={['ADMIN', 'USER']}>
+            <MyBookings/>
+        </PrivateRoute>}/>
+
     </Routes>
 
 }
